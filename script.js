4133b8cd5e49c965b7458d30c452eb0b9dd2ac78
codebykundan <kundanghugul386@gmail.com>
2025-06-29 15:28:47 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,8 @@
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+function openMenu(){
+    nav.style.right = "0";
+}
+function closeMenu(){
+    nav.style.right = "-180px";
+}
